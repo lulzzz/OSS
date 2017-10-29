4afe760106dda16cc722284be5b17e378fda788c
Anduin2017 <Anduin@aiursoft.com>
2017-10-29 23:54:24 +0800

    compress in a new way

--- Services/ImageCompresser.cs
+++ Services/ImageCompresser.cs
@@ -10,17 +10,9 @@ namespace Aiursoft.OSS.Services
         private readonly char _ = Path.DirectorySeparatorChar;
         public byte[] Compress(string path, string realname, int width, int height)
         {
-            var CompressedFolder = GetCurrentDirectory() + $"{_}Storage{_}_Compressed{_}";
-            if (Exists(CompressedFolder) == false)
-            {
-                CreateDirectory(CompressedFolder);
-            }
-
-            var realImagePath = CompressedFolder + realname;
-            File.Copy(path, realImagePath, true);
-            
+            var CompressedFolder = GetCurrentDirectory() + $"{_}Compressed{_}";
             var CompressedImagePath = $"{CompressedFolder}c_w{width}h{height}{realname}";
-            GetReducedImage(realImagePath, CompressedImagePath, width, height);
+            GetReducedImage(path, CompressedImagePath, width, height);
             return System.IO.File.ReadAllBytes(CompressedImagePath);
         }
         public void GetReducedImage(string sourceImage, string saveTarget, int width, int height)
