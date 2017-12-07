8b6f00cbc9d6dda7f7182bc5ffbcaa85db14e5d2
Anduin <Anduin@aiursoft.com>
2017-12-07 16:22:23 +0800

    Upgraded api level

--- Startup.cs
+++ Startup.cs
@@ -54,7 +54,6 @@ namespace Aiursoft.OSS
             }
 
             app.UseStaticFiles();
-            app.UseAiursoftAuthentication("6da19556352c16d8f87056c6f9cee746", "2cc99eb8d8be181244cb9c780b83ef2d");
             app.UseMvcWithDefaultRoute();
         }
     }
